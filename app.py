99e574f142dbe88de0dfde0fadc6b4e7e0538ea5
Artur <artur2005456@gmail.com>
2025-02-10 19:24:16 +0200

    Move Keys to weather_api

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template, request
-import requests
+from flask import Flask, render_template, request, jsonify
+from weather_api import get_weather  
 
 app = Flask(__name__)
 
-API_KEY = "6e1af1e6c02bb73d5bab72a8285d922c"  # OpenWeatherMap API ключ
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
-
-@app.route("/", methods=["GET", "POST"])
-def index():    
+@app.route("/")
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
---
+++ weather_api.py
@@ -0,0 +1,8 @@
+import requests
+
+OPENWEATHER_API_KEY = "6e1af1e6c02bb73d5bab72a8285d922c"
+OPENWEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+WEATHERAPI_KEY = "3e45c55430d64740bef182024250802"
+WEATHERAPI_URL = "http://api.weatherapi.com/v1/current.json"
+
