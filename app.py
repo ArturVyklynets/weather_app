6851d107397ebfe6fe40830432c2eb2034efe7cc
Daniel <daniel.nakushnov@gmail.com>
2025-02-11 19:17:47 +0200

    Created a basic get_weather function and a way to view the data through /weather

--- app.py
+++ app.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+# api testing, currently simply outputs json data
+# /weather?city=kyiv&country=ukraine?api=weatherapi
+# /weather?city=ivano-frankivsk&country=ukraine
+@app.route("/weather", methods=["GET"])
+def weather():
+    city = request.args.get("city")
+    country = request.args.get("country")
+    api = request.args.get("api")
+    api = api.lower() if api else "openweather"
+    result = get_weather(city, country, api)
+    return jsonify(result)
+
 if __name__ == "__main__":
     app.run(debug=True)
--- weather_api.py
+++ weather_api.py
@@ -6,3 +6,23 @@ OPENWEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
 WEATHERAPI_KEY = "3e45c55430d64740bef182024250802"
 WEATHERAPI_URL = "http://api.weatherapi.com/v1/current.json"
 
+def get_weather(city, country, api="openweather"):
+    if api == "openweather":
+        params = {
+            "q": f"{city},{country}",
+            "appid": OPENWEATHER_API_KEY,
+            "units": "metric"
+        }
+        response = requests.get(OPENWEATHER_URL, params=params)
+        data = response.json()
+        return data
+    elif api == "weatherapi":
+        params = {
+            "key": WEATHERAPI_KEY,
+            "q": f"{city},{country}"
+        }
+        response = requests.get(WEATHERAPI_URL, params=params)
+        data = response.json()
+        return data["current"]
+    else:
+        return None
