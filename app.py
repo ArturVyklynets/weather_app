746a452053bb37e567c8e74fb65ed50dd025a83e
Artur <artur2005456@gmail.com>
2025-02-13 12:38:17 +0200

    modify weather.html

--- app.py
+++ app.py
@@ -22,17 +22,19 @@ def weather():
     country = request.args.get("country")
     api = request.args.get("api")
     api = api.lower() if api else "openweather"
+    
     weather_data = get_weather(city, country, api)
+    
     forecast_data = [
-        {"date": "Понеділок", "time": "10:05", "icon": "sunny.png", "temp_min": 16, "temp_max": 26},
-        {"date": "Вівторок", "time": "10:05", "icon": "sunny.png", "temp_min": 16, "temp_max": 26},
-        {"date": "Середа", "time": "10:05", "icon": "cloudy.png", "temp_min": 18, "temp_max": 28},
-        {"date": "Четвер", "time": "10:05", "icon": "rainy.png", "temp_min": 14, "temp_max": 22},
-        {"date": "П'ятниця", "time": "10:05", "icon": "windy.png", "temp_min": 12, "temp_max": 20},
-        {"date": "Субота", "time": "10:05", "icon": "storm.png", "temp_min": 10, "temp_max": 18},
-        {"date": "Неділя", "time": "10:05", "icon": "snowy.png", "temp_min": 5, "temp_max": 10},
+        {"date": "Понеділок", "time": "10:05", "icon": "sunny.png", "temp_min": 16, "temp_max": 26, "sunrise": "07:16", "sunset": "17:03", "pressure": "743", "humidity": "73%", "wind": "7.3", "precipitation": "10%"},
+        {"date": "Вівторок", "time": "10:05", "icon": "sunny.png", "temp_min": 16, "temp_max": 26, "sunrise": "07:16", "sunset": "17:03", "pressure": "743", "humidity": "73%", "wind": "7.3", "precipitation": "10%"},
+        {"date": "Середа", "time": "10:05", "icon": "cloudy.png", "temp_min": 18, "temp_max": 28, "sunrise": "07:16", "sunset": "17:03", "pressure": "743", "humidity": "73%", "wind": "7.3", "precipitation": "10%"},
+        {"date": "Четвер", "time": "10:05", "icon": "rainy.png", "temp_min": 14, "temp_max": 22, "sunrise": "07:16", "sunset": "17:03", "pressure": "743", "humidity": "73%", "wind": "7.3", "precipitation": "10%"},
+        {"date": "П'ятниця", "time": "10:05", "icon": "cloudy.png", "temp_min": 18, "temp_max": 28, "sunrise": "07:16", "sunset": "17:03", "pressure": "743", "humidity": "73%", "wind": "7.3", "precipitation": "10%"},
+        {"date": "Субота", "time": "10:05", "icon": "rainy.png", "temp_min": 14, "temp_max": 22, "sunrise": "07:16", "sunset": "17:03", "pressure": "743", "humidity": "73%", "wind": "7.3", "precipitation": "10%"},
+        {"date": "Неділя", "time": "10:05", "icon": "sunny.png", "temp_min": 16, "temp_max": 26, "sunrise": "07:16", "sunset": "17:03", "pressure": "743", "humidity": "73%", "wind": "7.3", "precipitation": "10%"},
     ]
-
+    
     weather_info = {
         "city": weather_data["name"],
         "country": weather_data["sys"]["country"],
@@ -41,7 +43,6 @@ def weather():
     }
 
     return render_template("weather.html", weather=weather_info)
-
 
 # Обробка погоди для виведення на мапу
 @app.route("/weather_all")
